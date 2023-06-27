16a134ffab088585a90e5a997417f756256fd3dc
Rusik-hub <pluxindyu@mail.ru>
2023-06-27 12:35:38 +0500

    I added my first project on github

---
+++ Assets/Scripts/CreateEnemy.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateEnemy : MonoBehaviour
+{
+    private Transform[] _points;
+    [SerializeField] private Rigidbody _enemy;
+    [SerializeField] private float _respawnTime;
+    [SerializeField] private float _runningTime;
+
+    private void Start()
+    {
+        _points = new Transform[transform.childCount];
+
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            _points[i] = transform.GetChild(i);
+        }
+    }
+
+    private void Update()
+    {
+        _runningTime += Time.deltaTime;
+
+        if (_runningTime >= _respawnTime)
+        {
+            int numberOfRespawn = Random.Range(0, _points.Length);
+            Instantiate(_enemy, _points[numberOfRespawn].transform);
+            _runningTime = 0;
+        }
+    }
+}
